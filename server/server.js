96e7090f7124fde9ee496f230e0b54dfc48e4c81
Tom Main <tommain1498@gmail.com>
2024-10-26 23:49:33 +0100

    started on auth flow

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import pg from "pg";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url"; // Import fileURLToPath from url module
+import SpotifyWebApi from "spotify-web-api-node";
 
 // Define __dirname
 const __filename = fileURLToPath(import.meta.url);
@@ -17,6 +18,13 @@ app.use(express.static(join(__dirname, "public")));
 
 // Load environment variables
 env.config();
+
+// Spotify API
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_SECRET,
+    redirectUri: process.env.REDIRECT_URI
+  });
 
 // // Database connection
 // const db = new pg.Client({
@@ -34,6 +42,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+
+
+
+app.get("/api/login", async (req, res) => {
+    const scopes = ['user-read-private', 'user-read-email', 'user-read-playback-state', 'user-modify-playback-state', 'streaming']
+    res.send(spotifyApi.createAuthorizeURL(scopes));
+    // res.send("Hello World");
+});
+
+
+
+
+
 
 // ==============================
 // Start the server
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import Login from './pages/Login'
+import ErrorPage from './pages/ErrorPage' 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",	
+    element: <Login />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <RouterProvider router={router} />
+  </>
 )
