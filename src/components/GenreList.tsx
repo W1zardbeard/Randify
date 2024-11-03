24de2e42b16ac763652149b7313cd66e725299e4
Tom Main <tommain1498@gmail.com>
2024-11-03 17:23:50 +0000

    finished side nav and top header, reworked auth and refresh token

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,7 @@
+export default function GenreList() {
+    return (
+        <div className="genreList">
+            <h1>Genre List</h1>
+        </div>
+    )
+}
--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -1,12 +1,14 @@
+import UserProfile from './UserProfile';
+
+
 export default function TopHeader(props: any) {
     return (
         <div className="topHeader">
             <h1>{props.pageTitle}</h1>
 
-            <div className="userProfile">
-                <img src="https://placehold.co/50x50" alt="user" />
-                <h3>John Doe</h3>
-            </div>
+            <UserProfile
+                user={props.user}
+            />
         </div>
     )
 }
